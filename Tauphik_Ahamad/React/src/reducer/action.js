8d1540aaff9d49b2a52a763d3440442f8dcf0463
Tauphik Ahamad <AL10940@accionlabs.in>
2023-08-08 11:40:45 +0530

    React reducer

---
+++ Tauphik_Ahamad/React/src/reducer/action.js
@@ -0,0 +1,18 @@
+// function that perform/return type of action
+/**
+ * 1. PLUS
+ * 2. MINUS
+ *
+ */
+
+export const countPlus = () => {
+  return {
+    type: "PLUS",
+  };
+};
+
+export const countMinus = () => {
+  return {
+    type: "MINUS",
+  };
+};
